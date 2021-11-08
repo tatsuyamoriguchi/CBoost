10a5d2fdd4ac5a01ae6cc320decb84a69fff3c44
Brian Moriguchi <brian@beckos.com>
2021-11-07 21:21:18 -0800

    Initial Commit

---
+++ CBoost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CBoost
+//
+//  Created by Tatsuya Moriguchi on 11/7/21.
+//  Copyright © 2021 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
